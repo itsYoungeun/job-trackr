55de8fa3663a220c8114dac279b1846503c1d7e2
Peter <youngeunpc@yahoo.com>
2025-04-18 20:47:18 -0300

    Fixed bug for backend not running

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import imageRoute from './routes/imageRoute';
+import imageRoute from './routes/imageRoute.ts';
 
 const app = express();
 app.use(cors());
