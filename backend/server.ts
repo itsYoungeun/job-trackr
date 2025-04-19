fc04d9198bdfb9e4b1be29d59b30e0b3efd0ff8b
Peter <youngeunpc@yahoo.com>
2025-04-19 16:32:06 -0300

    Fixed backend route

--- backend/server.ts
+++ backend/server.ts
@@ -8,5 +8,9 @@ app.use(express.json());
 
 app.use('/api/image', imageRoute);
 
+app.get('/', (_req, res) => {
+  res.send('API is running...');
+});
+
 const PORT = process.env['PORT'] || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
