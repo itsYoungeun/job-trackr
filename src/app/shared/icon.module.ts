708a671a111960a1fec13525652b5a63edab7d9d
Peter <youngeunpc@yahoo.com>
2025-05-17 22:23:31 -0300

    Fixed trash icon not showing up

--- src/app/shared/icon.module.ts
+++ src/app/shared/icon.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
-import { Building2, Calendar, Camera, CircleUser, Funnel, HandCoins, LayoutGrid, List, MapPin, Pencil } from 'lucide-angular';
+import { Building2, Calendar, Camera, CircleUser, Funnel, HandCoins, LayoutGrid, List, MapPin, Pencil, Trash2 } from 'lucide-angular';
 
 @NgModule({
-  imports: [LucideAngularModule.pick({ Building2, Calendar, Camera, CircleUser, Funnel, HandCoins, LayoutGrid, List, MapPin, Pencil })],
+  imports: [LucideAngularModule.pick({ Building2, Calendar, Camera, CircleUser, Funnel, HandCoins, LayoutGrid, List, MapPin, Pencil, Trash2 })],
   exports: [LucideAngularModule],
 })
 export class IconModule {}
